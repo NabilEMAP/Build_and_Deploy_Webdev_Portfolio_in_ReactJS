83beee7dbb347565537ee163fa6398ee9ab86fda
NabilEMAP <Nabil_EM@outlook.com>
2023-06-18 17:45:49 +0200

    Using useEffect for 3D Model mobile mediaQuery

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -25,7 +25,7 @@ const Computers = ({ isMobile }) => {
         scale={isMobile ? 0.7 : 0.75 }
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         //scale={isMobile ? 0.35 : 0.75 }
-        //position={isMobile ? [0, -2, -0.35] : [0, -3.25, -1.5]}
+        //position={isMobile ? [0, -1.5, -0.35] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
